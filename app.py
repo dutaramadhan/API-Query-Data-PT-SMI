c8b240b702b3cf8cc2ae6bacff6df94c7d8d0f60
dutaramadhan <duta2711@gmail.com>
2024-01-16 10:39:31 +0700

    fix

--- app.py
+++ app.py
@@ -38,22 +38,25 @@ def getEmbeddings(text):
 @app.route('/smi/api/embedding/query', methods=['GET'])
 def get_data():
     query = request.args.get('query')
-    query_embedding = getEmbeddings(query)
-    query_vector = query_embedding['data'][0]['embedding']
-    results = model.vector_search(query_vector)
+    try:
+        query_embedding = getEmbeddings(query)
+        query_vector = query_embedding['data'][0]['embedding']
+        results = model.vector_search(query_vector)
     
-    response_data = []
-    for result in results:
-        entry = {
-            "similarity": result[0],
-            "id": result[1],
-            "source_title": result[2],
-            "content": result[3]
-        }
-        response_data.append(entry)
+        response_data = []
+        for result in results:
+            entry = {
+                "similarity": result[0],
+                "id": result[1],
+                "source_title": result[2],
+                "content": result[3]
+            }
+            response_data.append(entry)
 
-    response_data = {"result": response_data}
-    return jsonify(response_data)
+        response_data = {"result": response_data}
+        return jsonify(response_data)
+    except Exception as e:
+        return e 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
